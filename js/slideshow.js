f7281edb9b9e5ec19779beeb5c82f14d44c50f88
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-03-08 21:58:40 +0630

    class-projects

--- js/slideshow.js
+++ js/slideshow.js
@@ -68,3 +68,25 @@ createSlideShow.addEventListener("click", () => {
   createCarousel(allPhoto.map((el) => el.src));
   //   createCarousel();
 });
+
+uploaderUI.addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+
+uploaderUI.addEventListener("drop", (event) => {
+  event.preventDefault();
+  console.log(event.dataTransfer.files);
+
+  [...event.dataTransfer.files].forEach((file) => {
+    // const img = document.createElement("img");
+    const img = new Image();
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      console.log(event.target);
+      img.src = event.target.result;
+      img.classList.add("photo", "me-2");
+      photos.append(img);
+    });
+    reader.readAsDataURL(file);
+  });
+});
